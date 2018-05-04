e08fec5abaa5677ba3bc7042814bfb58b463a17e
pvarchenko <pvarchenko@alfabank.ru>
2018-05-04 16:20:38 +0300

    quick sort

--- src/sorting2/QuickSort.java
+++ src/sorting2/QuickSort.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] values = {0, 12, 11, 13, 6, 5, 7};
+        int[] values = {77, 0, 12, 11, 13, 6, 5, 7};
 
         sort(values, 0, values.length - 1);
         System.out.println(Arrays.toString(values));
@@ -23,28 +23,28 @@ public class QuickSort {
     public static int partition(int[] arr, int l, int r) {
         int pivot = arr[r];
         int i = l - 1;
-        for (int j = l; j < r; j++) {
-            if (arr[j] <= pivot) {
+        for (int j = l; j <r; j++){
+            if(pivot >= arr[j]){
                 i++;
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[r];
+
+        int temp = arr[i+1];
+        arr[i+1] = arr[r];
         arr[r] = temp;
 
-        return i + 1;
+        return i+1;
     }
 
     public static void sort(int[] arr, int l, int r) {
         if (l < r) {
-            int pi = partition(arr,l,r);
+            int pi = partition(arr, l, r);
 
-            sort(arr, l, pi-1);
-            sort(arr, pi+1, r);
+            sort(arr, l, pi - 1);
+            sort(arr, pi + 1, r);
         }
     }
 }
--- src/sorting2/Train.java
+++ src/sorting2/Train.java
@@ -14,35 +14,34 @@ public class Train {
         insertion(values.clone());
         selection(values.clone());
         mergeSort(values.clone());
+        quickSort(values.clone());
     }
 
     private static void selection(int[] values) {
-
         for (int j = 0; j < values.length; j++) {
-            int min = j;
-            for (int i = j + 1; i < values.length; i++) {
-                if (values[i] < values[min]) {
-                    min = i;
+            int minIdx = j;
+            for (int i = j; i < values.length; i++) {
+                if (values[minIdx] > values[i]) {
+                    minIdx = i;
                 }
             }
-            int temp = values[min];
-            values[min] = values[j];
-            values[j] = temp;
+            int temp = values[j];
+            values[j] = values[minIdx];
+            values[minIdx] = temp;
         }
 
         System.out.println(Arrays.toString(values));
     }
 
     private static void insertion(int[] values) {
-
         for (int i = 1; i < values.length; i++) {
-            int curVal = values[i];
+            int curEl = values[i];
             int j = i - 1;
-            while (j >= 0 && curVal < values[j]) {
+            while (j >= 0 && curEl < values[j]) {
                 values[j + 1] = values[j];
                 j--;
             }
-            values[j + 1] = curVal;
+            values[j + 1] = curEl;
         }
 
         System.out.println(Arrays.toString(values));
@@ -64,12 +63,11 @@ public class Train {
     private static void mergeSort(int[] arr) {
         mergeSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
-
     }
 
     private static void mergeSort(int[] arr, int l, int r) {
         if (l < r) {
-            int m = (r + l) / 2;
+            int m = (l + r) / 2;
             mergeSort(arr, l, m);
             mergeSort(arr, m + 1, r);
             merge(arr, l, m, r);
@@ -80,20 +78,18 @@ public class Train {
         int s1 = m - l + 1;
         int s2 = r - m;
 
-        int[] L = new int[s1];
-        int[] R = new int[s2];
+        int L[] = new int[s1];
+        int R[] = new int[s2];
 
         for (int i = 0; i < s1; i++) {
-            L[i] = arr[i + l];
+            L[i] = arr[l + i];
         }
-
         for (int i = 0; i < s2; i++) {
-            R[i] = arr[i + m + 1];
+            R[i] = arr[m + i + 1];
         }
 
         int i = 0, j = 0;
         int k = l;
-
         while (i < s1 && j < s2) {
             if (L[i] <= R[j]) {
                 arr[k] = L[i];
@@ -107,13 +103,47 @@ public class Train {
 
         while (i < s1) {
             arr[k] = L[i];
-            k++;
             i++;
+            k++;
         }
+
         while (j < s2) {
             arr[k] = R[j];
-            k++;
             j++;
+            k++;
         }
     }
+
+
+    private static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static int partition(int[] arr, int l, int r) {
+        int pi = arr[r];
+        int i = l-1;
+        for (int j = l; j < r; j++) {
+            if(arr[j] <= pi){
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[r];
+        arr[r] = temp;
+
+        return i+1;
+    }
+
+    private static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int pi = partition(arr, l, r);
+            quickSort(arr, l, pi - 1);
+            quickSort(arr, pi + 1, r);
+        }
+    }
+
 }
